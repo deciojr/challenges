3fab645e9fadf32d094994ad953cf6b325e2dd8f
Décio Júnior <deciojunior1998@gmail.com>
2020-03-14 17:26:33 -0300

    Adicionar porta do backend as configurações de ambiente

--- dev/backend/src/main.ts
+++ dev/backend/src/main.ts
@@ -4,7 +4,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  let app = await NestFactory.create(AppModule);
+
+  await app.listen(process.env.SERVER_PORT);
 }
 bootstrap();
