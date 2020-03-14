178a448ee0fc172c3f2943efbc9ef566667af322
Décio Júnior <deciojunior1998@gmail.com>
2020-03-14 20:45:09 -0300

    Adicionar pipe global de validação de DTOs

--- dev/backend/src/main.ts
+++ dev/backend/src/main.ts
@@ -3,8 +3,29 @@ dotenv.config({ path: '../.env' });
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const addToApp = (
+  app: INestApplication,
+  toUse,
+  type: 'pipe' | 'filter' | 'interceptor' | 'guard',
+  global = false,
+) => {
+  if (global) {
+    const addGlobally = {
+      pipe: pipe => app.useGlobalPipes(pipe),
+      filter: filter => app.useGlobalFilters(filter),
+      interceptor: interceptor => app.useGlobalInterceptors(interceptor),
+      guard: guard => app.useGlobalGuards(guard),
+    };
+
+    return addGlobally[type](toUse);
+  }
+
+  return app.use(toUse);
+};
 async function bootstrap() {
   let app = await NestFactory.create(AppModule);
+
+  app = addToApp(app, new ValidationPipe(), 'pipe', true);
 
   await app.listen(process.env.SERVER_PORT);
 }
