d1ef1b421a0078a8d84a2e8f1394db9fb1b1422f
Décio Júnior <deciojunior1998@gmail.com>
2020-03-14 20:48:22 -0300

    Adicionar dependências e configurar módulo do Swagger

--- dev/backend/src/main.ts
+++ dev/backend/src/main.ts
@@ -1,6 +1,10 @@
 import * as dotenv from 'dotenv';
 dotenv.config({ path: '../.env' });
+
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 const addToApp = (
@@ -22,10 +26,29 @@ const addToApp = (
 
   return app.use(toUse);
 };
+
+const setupSwagger = (app: INestApplication) => {
+  const options = new DocumentBuilder()
+    .setTitle('ZRP Dev Challenge - ONU Threats')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+
+  SwaggerModule.setup('api', app, document);
+};
+
 async function bootstrap() {
   let app = await NestFactory.create(AppModule);
 
-  app = addToApp(app, new ValidationPipe(), 'pipe', true);
+  setupSwagger(app);
+
+  app = addToApp(
+    app,
+    new ValidationPipe({ disableErrorMessages: true }),
+    'pipe',
+    true,
+  );
 
   await app.listen(process.env.SERVER_PORT);
 }
