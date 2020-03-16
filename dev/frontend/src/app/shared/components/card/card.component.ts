20166fcfa0454d0f7d5528c928bb52f13e1c1217
Décio Júnior <deciojunior1998@gmail.com>
2020-03-16 14:09:38 -0300

    Remover scss|spec do componente card

--- dev/frontend/src/app/shared/components/card/card.component.ts
+++ dev/frontend/src/app/shared/components/card/card.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy, Input } from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CardComponent {
